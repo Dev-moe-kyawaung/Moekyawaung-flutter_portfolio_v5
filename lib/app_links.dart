1cf4abd373b91c9d657fb7456e3fcd41202f5d31
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-10 18:50:30 +0630

    Add files via upload

---
+++ lib/app_links.dart
@@ -0,0 +1,193 @@
+class AppLinks {
+  AppLinks._();
+
+  // === GRAVATAR ===
+  static const gravatar     = 'https://gravatar.com/moekyawaung2026';
+  static const gravatarApi  = 'https://gravatar.com/moekyawaung13721';
+  static const avatarUrl    = 'https://www.gravatar.com/avatar/moekyawaung2026?s=400&d=identicon';
+
+  // === CLOUDINARY IMAGES ===
+  static const profileMain = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778527878/IMG_20260430_053105_uef0yr.png';
+  static const profileAlt1 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778747388/image-1_1_khsx9s.png';
+  static const profileAlt2 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778763535/MKA_25_lbx6fb.webp';
+  static const profileAlt3 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778763531/MKA_12_iv8kpm.webp';
+  static const profileAlt4 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778747384/image-1_f6zlmk.jpg';
+  static const profileAlt5 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778747384/image_1_buwgls.png';
+  static const profileAlt6 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778763531/MKA_3_zqrhhr.webp';
+  static const profileAlt7 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778763532/MKA_11_jbijtv.webp';
+  static const profileAlt8 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778763532/MKA_13_i4bao3.webp';
+  static const profileAlt9 = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778763536/preview_ls5ptn.webp';
+  static const heroPreview  = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795825/preview_dzhqvv.webp';
+  static const bgImage1    = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795799/2024119_20_b94fen.jpg';
+  static const bgImage2    = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795800/2024119_18_syk2ou.jpg';
+  static const bgImage3    = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795800/2024119_12_sqhcat.jpg';
+  static const fireworks   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1779052645/2153-fireworks-composer_gm3e0h.jpg';
+
+  // Copilot generated images
+  static const copilot1   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795856/copilot_image_1778795675037_heh9xk.png';
+  static const copilot2   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795856/copilot_image_1778794626112_ega7kk.png';
+  static const copilot3   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795859/copilot_image_1778794430377_n7xlmz.png';
+  static const copilot4   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795856/copilot_image_1778795000722_eo96gj.png';
+  static const copilot5   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795829/copilot_image_1778795000722_okryxj.png';
+  static const copilot6   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795847/copilot_image_1778795115579_acfm5j.png';
+  static const copilot7   = 'https://res.cloudinary.com/dye5qpwii/image/upload/v1778795853/copilot_image_1778794781671_kytvkc.png';
+
+  // Videos
+  static const video1 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779031596/Javier_Pardina_10_wttux4.mp4';
+  static const video2 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779052711/Javier_Black-Dark-Ring.mp4';
+  static const video3 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779052704/Javier_Pardina_10_ay7iai.mp4';
+  static const video4 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779052708/AUDI_-_Javier_Pardina_1_gavyon.mp4';
+  static const video5 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779052732/Javier_Pardina_2_l1mtud.mp4';
+  static const video6 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779031657/COACH_-_Javier_Pardina_gdjsjg.mp4';
+  static const video7 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779031569/Javier_Pardina_8_r1lgpj.mp4';
+  static const video8 = 'https://res.cloudinary.com/dye5qpwii/video/upload/v1779031566/Javier_Pardina_11_r5y8no.mp4';
+
+  // === MAIN GITHUB ===
+  static const githubMain  = 'https://github.com/Dev-moe-kyawaung/';
+  static const githubAlt   = 'https://github.com/Moekyawaung-mka';
+  static const featuredRepo= 'https://github.com/Dev-moe-kyawaung/pulsesync-android';
+
+  // === GITHUB ACCOUNT COLLECTION (43 accounts) ===
+  static const List<String> githubAccounts = [
+    'https://moekyawaung-china.github.io/',
+    'https://moekyawaung-developer.github.io/',
+    'https://moekyawaungvivov30pro-design.github.io/',
+    'https://moekyaw-aung-mm.github.io/',
+    'https://moekyawaung-mk.github.io/',
+    'https://moekyawaung-microsoft.github.io/',
+    'https://moekyawaung-cyber.github.io/',
+    'https://moekyawaung-bangkok.github.io/',
+    'https://moekyawaung-micro.github.io/',
+    'https://moekyawaungmka2032-boop.github.io/',
+    'https://moekyawaung-dev-mm.github.io/',
+    'https://moekyaw-developer.github.io/',
+    'https://moekyawaung.github.io/',
+    'https://Moekyawaung-mm.github.io/',
+    'https://moekyawaung-tech.github.io/',
+    'https://moekyawaung-hack.github.io/',
+    'https://moekyawaung-graduate.github.io/',
+    'https://Moekyawaung-Linux.github.io/',
+    'https://Moekyawaung-coder.github.io/',
+    'https://moekyawaung-designer.github.io/',
+    'https://Moekyawaung2026.github.io/',
+    'https://moekyawaungmka2034-coder.github.io/',
+    'https://moekyawaung-web.github.io/',
+    'https://Moekyawaung-dev.github.io/',
+    'https://MoeKyawAung-code.github.io/',
+    'https://moekyawaung-creator.github.io/',
+    'https://moekyawaung-webdeveloper.github.io/',
+    'https://Moekyawaung-co.github.io/',
+    'https://moekyawaung-edu.github.io/',
+    'https://moekyawaung-senior.github.io/',
+    'https://Moekyawaung-Development.github.io/',
+    'https://moekyawaung-google.github.io/',
+    'https://Moe-KyawAung.github.io/',
+    'https://moekyawaung-tech.github.io/',
+    'https://moekyawaung-hack.github.io/',
+    'https://moekyawaung-graduate.github.io/',
+    'https://moekyawaung-micro.github.io/',
+    'https://moekyawaung-china.github.io/',
+    'https://moekyawaungmka2032-boop.github.io/',
+    'https://moekyawaung-bangkok.github.io/',
+    'https://moekyawaung-designer.github.io/',
+    'https://moekyawaung-mk.github.io/',
+    'https://moekyawaung-microsoft.github.io/',
+  ];
+
+  // === LOVABLE PWA COLLECTION (38+) ===
+  static const List<String> lovableApps = [
+    'https://happy-cv-creator.lovable.app',
+    'https://moekyawaung.lovable.app',
+    'https://moekyawaungmybio.lovable.app/',
+    'https://the-cv-palette.lovable.app',
+    'https://moekyaw-url.lovable.app',
+    'https://moekyawaung-dev.lovable.app',
+    'https://moe-kyaw-aung.lovable.app',
+    'https://moekyawaungmka.lovable.app',
+    'https://moekyaw.lovable.app',
+    'https://m-moekyaw.lovable.app',
+    'https://dev-moekyawaung.lovable.app',
+    'https://dev-moekyaw.lovable.app',
+    'https://cv-beacon.lovable.app/',
+    'https://preview--moekyawaungmkamka.lovable.app/',
+    'https://moekyawaungmkamka.lovable.app',
+    'https://pixel-perfect-snap-39.lovable.app',
+    'https://devmoekyaw.lovable.app',
+    'https://profile-persuasion-hub.lovable.app',
+    'https://friendly-haven-io.lovable.app',
+    'https://moekyawaung-github.lovable.app',
+    'https://moekyawgithub.lovable.app',
+    'https://joy-codify-life.lovable.app/',
+    'https://mmoekyaw.lovable.app',
+    'https://color-code-chronicles.lovable.app',
+    'https://moekyawaung-free.lovable.app',
+    'https://app-skill-gallery.lovable.app',
+    'https://spark-coach-create.lovable.app',
+    'https://moekyaw-mk.lovable.app',
+    'https://moekyawaung-myanmar.lovable.app',
+    'https://mmoe.lovable.app',
+    'https://moekyaw-dev.lovable.app',
+    'https://moekyawaung-github.lovable.app',
+    'https://moekyawaung-dev.lovable.app',
+    'https://m-moekyaw.lovable.app',
+    'https://the-cv-palette.lovable.app',
+    'https://joy-codify-life.lovable.app/',
+    'https://moekyawaung-github.lovable.app',
+    'https://moekyawaung-myanmar.lovable.app',
+  ];
+
+  // === EMAIL COLLECTION (21) ===
+  static const List<String> emails = [
+    'moekyawaung@programmer.net',
+    'moekyawaung@collector.org',
+    'moekyawaung@technologist.com',
+    'moekyawaung@techie.com',
+    'moekyawaung@graphic-designer.com',
+    'moekyawaung@cybergal.com',
+    'moekyawaung@webname.com',
+    'moekyawaung@hackermail.com',
+    'moekyawaung@graduate.org',
+    'moekyawaung@engineer.com',
+    'moekyawaung@asia.com',
+    'moekyawaung@contractor.net',
+    'moekyawaung@linuxmail.org',
+    'moekyawaung@usa.com',
+    'moekyawaung@europe.com',
+    'moekyawaung@mail.com',
+    'moekyawaung@iname.com',
+    'moekyawaung@socialogist.com',
+    'moekyawaung@secretary.net',
+    'moekyawaung@publicist.com',
+  ];
+
+  // === SOCIAL LINKS ===
+  static const linkedin  = 'https://www.linkedin.com/in/moe-kyaw-aung-2653093a1';
+  static const youtube   = 'https://www.youtube.com/channel/UCuTXUguZb4xjeL2nX8WJG';
+  static const tumblr    = 'https://www.tumblr.com/moekyawaung';
+  static const flickr    = 'https://www.flickr.com/people/204037451@N06';
+  static const bluesky   = 'https://bsky.app/profile/moekyawaung96.bsky.social';
+  static const vimeo     = 'https://vimeo.com/user252414232';
+  static const gravatar2 = 'https://gravatar.com/moekyawaung13721';
+  static const slack     = 'https://moekyawaung.slack.com/';
+  static const strikingly= 'http://moekyawaung2026.strikingly.com';
+
+  // === PROJECT REPOS ===
+  static const videoPlayer    = 'https://github.com/moekyawaung-tech/video-player';
+  static const socialDashboard= 'https://github.com/moekyawaung-tech/social-dashboard';
+  static const gameCollection = 'https://github.com/moekyawaung-tech/game-collection';
+  static const pwaApp         = 'https://github.com/moekyawaung-tech/pwa-app';
+  static const jobPortal      = 'https://github.com/moekyawaung-tech/Job-Portal-App';
+  static const posFull        = 'https://github.com/moekyawaung-tech/POS-Full-Version';
+  static const todoApp        = 'https://github.com/moekyawaung-tech/javascript-todo';
+  static const thailandTravel = 'https://github.com/moekyawaung-tech/thailand-travel';
+  static const casinoApp      = 'https://github.com/moekyawaung-tech/casino-app';
+  static const snakeGame      = 'https://github.com/moekyawaung-tech/Snake-Game-App';
+  static const posAdvance     = 'https://github.com/moekyawaung-tech/Advance-POS-Version';
+  static const posUltimate    = 'https://github.com/moekyawaung-tech/POS-Ultimate-Version';
+  static const posUltimatePro = 'https://github.com/moekyawaung-tech/POS-Ultimate-Pro-Max';
+  static const postcodeCyber  = 'https://github.com/Moekyawaung-cyber/My_postcode-My-web_project';
+  static const hospitalLists  = 'https://github.com/Moekyawaung-cyber/Hospital-Lists';
+  static const weatherApp     = 'https://github.com/moekyawaung-tech/Weather-app';
+  static const dailyPlanner   = 'https://github.com/moekyawaung-tech/Daily-planner-app';
+  static const lensLite       = 'https://github.com/moekyawaung-tech/Lens-lite';
+}
